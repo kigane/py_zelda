d71222522ad2cdcbe9ed0b3bc3e3a54d3cf58497
kigane <huwkigane@outlook.com>
2022-02-20 14:32:01 +0800

    attack+animation

--- code/player.py
+++ code/player.py
@@ -1,28 +1,89 @@
+import os
 import pygame 
+from debug import debug
+from utils import import_folder
 from settings import *
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups,obstacle_sprites):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft = pos)
+		self.rect = self.image.get_rect(topleft=pos)
 		self.hitbox = self.rect.inflate(0, -26)
-
+		self.state = "down"
+		self.frame_index = 0
+		self.anim_spd = 0.15
+		# graphic set up
+		self.import_player_assets()
+		# move
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
-
+		# collision
 		self.obstacle_sprites = obstacle_sprites
+		# attack
+		self.attacking = False
+		self.atk_cooldown = 400
+		self.atk_time = None
+
+	def import_player_assets(self):
+		chara_path = "../graphics/player"
+		self.animations = {
+			'up': [], 'down': [], 'left': [], 'right': [],
+			'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': [],
+			'up_attack': [], 'down_attack': [], 'left_attack': [], 'right_attack': [],
+		}
+
+		for anim in self.animations.keys():
+			full_path = os.path.join(chara_path, anim)
+			self.animations[anim] = import_folder(full_path)
+
 
 	#* 处理输入信号
 	def input(self):
 		keys = pygame.key.get_pressed()
-		if keys[pygame.K_w]: self.direction.y = -1
-		elif keys[pygame.K_s]: self.direction.y = 1
-		else: self.direction.y = 0
 
-		if keys[pygame.K_a]: self.direction.x = -1
-		elif keys[pygame.K_d]: self.direction.x = 1
-		else: self.direction.x = 0
+		if keys[pygame.K_w]:
+			self.direction.y = -1
+			self.state = 'up'
+		elif keys[pygame.K_s]:
+			self.direction.y = 1
+			self.state = 'down'
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_a]:
+			self.direction.x = -1
+			self.state = 'left'
+		elif keys[pygame.K_d]:
+			self.direction.x = 1
+			self.state = 'right'
+		else:
+			self.direction.x = 0
+
+		if keys[pygame.K_j] and not self.attacking:
+			self.attacking = True
+			self.atk_time = pygame.time.get_ticks()
+		if keys[pygame.K_k] and not self.attacking:
+			self.attacking = True
+			self.atk_time = pygame.time.get_ticks()
+
+
+	def get_status(self):
+		if self.direction.x == 0 and self.direction.y == 0:
+			self.chage_state('idle')
+
+		if self.attacking:
+			self.direction.x = 0
+			self.direction.y = 0
+			self.chage_state('attack')
+
+
+	def chage_state(self, state: str):
+		if '_' in self.state:
+			self.state = self.state.split('_')[0]
+
+		self.state = self.state + '_' + state
+
 
 	#* 处理移动
 	def move(self, speed):
@@ -53,9 +114,28 @@ class Player(pygame.sprite.Sprite):
 					elif self.direction.y < 0:
 						self.hitbox.top = sprite.hitbox.bottom
 
+
+	def cooldowns(self):
+		curr_time = pygame.time.get_ticks()  # ms
+
+		if self.attacking and curr_time - self.atk_time >= self.atk_cooldown:
+			self.attacking = False
+
+
+	def animate(self):
+		anim = self.animations[self.state]
+		self.frame_index = (self.frame_index + self.anim_spd) % len(anim)
+		self.image = anim[int(self.frame_index)]
+		self.rect = self.image.get_rect(center=self.hitbox.center)
+
+
 	#* 实际调用各种处理方法，会被Group调用
 	def update(self):
 		self.input()
+		self.get_status()
+		debug(self.state)
 		self.move(self.speed)
+		self.cooldowns()
+		self.animate()
 
 
